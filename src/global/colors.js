316466f3427eb04ab7fa3ae5e1e8f62b2f9496f9
Alan Maldonado <alanmaldo.ct@gmail.com>
2024-03-14 17:40:43 -0300

    Eliminando cosas innecesarias

--- src/global/colors.js
+++ src/global/colors.js
@@ -1,8 +1,4 @@
 export const colors = {
-    blue_100: '#DCF2F1',
-    blue_200: '#7FC7D9',
-    blue_300: '#365486',
-    blue_400: '#0F1035',
     vermillion: '#dd403a',
     jet:"#3e363f",
     ivory:"#fffce8",
--- src/global/fonts.js
+++ src/global/fonts.js
@@ -1,6 +1,4 @@
 export const fonts = {
-    InterRegular: require('../../assets/fonts/Inter-Regular.ttf'),
-    InterBold: require('../../assets/fonts/Inter-Bold.ttf'),
     Comforta: require('../../assets/fonts/Comfortaa-VariableFont_wght.ttf'),
     Philo: require('../../assets/fonts/Philosopher-Regular.ttf'),
     PhiloBold: require('../../assets/fonts/Philosopher-Bold.ttf'),
