5a2502072b16fb274c71e7cf82d7d60a81b4644e
Alan Maldonado <alanmaldo.ct@gmail.com>
2024-03-18 11:13:42 -0300

    Detalles

--- src/db/index.js
+++ src/db/index.js
@@ -3,51 +3,50 @@ import * as SQLite from 'expo-sqlite';
 const db = SQLite.openDatabase('sessions.db');
 
 export const init = () => {
-    const promise = new Promise((resolve,reject) => {
+    const promise = new Promise((resolve, reject) => {
         db.transaction(tx => {
             tx.executeSql('CREATE TABLE IF NOT EXISTS sessions (localId TEXT PRIMARY KEY NOT NULL, email TEXT NOT NULL, token TEXT NOT NULL'),
-            [],
-            ()=> resolve(),
-            (_,error) => {reject(error)}
+                [],
+                () => resolve(),
+                (_, error) => { reject(error) }
         })
     })
     return promise
 }
-export const insertSession = ({email,localId,token}) => {
-    const promise = new Promise((accept,reject) => {
+export const insertSession = ({ email, localId, token }) => {
+    const promise = new Promise((accept, reject) => {
         db.transaction((tx) => {
             tx.executeSql(
                 'INSERT INTO sessions (email, localId , token) VALUES(?,?,?);',
-                [email,localId,token],
-                (_,result) => resolve(result),
-                (_, error)=> reject(error)
+                [email, localId, token],
+                (_, result) => resolve(result),
+                (_, error) => reject(error)
             )
         })
     })
     return promise;
 }
 
-export const fetchSessions = () =>{
-    const promise = new Promise((resolve,reject)=>{
-        db.transaction((tx) =>{
+export const fetchSessions = () => {
+    const promise = new Promise((resolve, reject) => {
+        db.transaction((tx) => {
             tx.executeSql(
-                'SELECT * FROM sessions',
+                'SELECT * FROM sessions'),
                 [],
-                (_,result) => resolve(result),
-                (_,error) => reject(error)
-            )
+                (_, result) => resolve(result),
+                (_, error) => reject(error)
         })
     })
     return promise
 }
-export const deleteSessions = ({localId}) => {
-    const promise = new Promise((accept,reject) => {
+export const deleteSessions = ({ localId }) => {
+    const promise = new Promise((accept, reject) => {
         db.transaction(tx => {
             tx.executeSql(
                 'DELETE FROM sessions WHERE localId = ?',
                 [localId],
-                (_,result) => resolve(result),
-                (_, error)=> reject(error)
+                (_, result) => resolve(result),
+                (_, error) => reject(error)
             )
         })
     })
